06d07df9333600bb620a4249e8102b64d8e505fa
Girish Pillai <girishpillai@hotmail.com>
2026-02-24 14:19:29 -0600

    Add AzureLocal-Node-Upgrade script for automatic agent upgrades

---
+++ azure_jumpstart_localbox/AzureLocal-Node-Upgrade.ps1
@@ -0,0 +1,12 @@
+Set-AzContext -Subscription "5f3972d8-b1e2-4a27-bf67-0db461308c53"
+
+$params = @{
+  ResourceGroupName = "RGAZLOCAL"
+  ResourceProviderName = "Microsoft.HybridCompute"
+  ResourceType = "Machines"
+  ApiVersion = "2024-05-20-preview"
+  Name = "AZLHOST2"
+  Method = "PATCH"
+  Payload = '{"properties":{"agentUpgrade":{ "enableAutomaticUpgrade":true}}}'
+}
+Invoke-AzRestMethod @params
