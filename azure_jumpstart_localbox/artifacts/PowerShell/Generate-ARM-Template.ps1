4aa4f5af971d34ddee852fbc67d7000a24adb7df
Girish Pillai <girishpillai@hotmail.com>
2026-03-14 17:48:02 -0500

    Refactor Generate-ARM-Template script to improve parameter handling and streamline role assignments

--- azure_jumpstart_localbox/artifacts/PowerShell/Generate-ARM-Template.ps1
+++ azure_jumpstart_localbox/artifacts/PowerShell/Generate-ARM-Template.ps1
@@ -1,92 +1,62 @@
-$WarningPreference = "SilentlyContinue"
-$ErrorActionPreference = "Stop"
+$WarningPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-# Set paths
-$Env:LocalBoxDir = "C:\LocalBox"
+$Env:LocalBoxDir = 'C:\LocalBox'
 
-# Import Configuration Module
 $LocalBoxConfig = Import-PowerShellDataFile -Path $Env:LocalBoxConfigFile
 Start-Transcript -Path "$($LocalBoxConfig.Paths.LogsDir)\Generate-ARM-Template.log"
 
-# Add necessary role assignments
-# $ErrorActionPreference = "Continue"
-# New-AzRoleAssignment -ObjectId $env:spnProviderId -RoleDefinitionName "Azure Connected Machine Resource Manager" -ResourceGroup $env:resourceGroup -ErrorAction Continue
-# $ErrorActionPreference = "Stop"
-
 $arcNodes = Get-AzConnectedMachine -ResourceGroup $env:resourceGroup
-$arcNodeResourceIds = $arcNodes.Id | ConvertTo-Json -AsArray
+$arcNodeResourceIds = @($arcNodes.Id)
 
-# foreach ($machine in $arcNodes) {
-#     $ErrorActionPreference = "Continue"
-#     New-AzRoleAssignment -ObjectId $machine.IdentityPrincipalId -RoleDefinitionName "Key Vault Secrets User" -ResourceGroup $env:resourceGroup
-#     New-AzRoleAssignment -ObjectId $machine.IdentityPrincipalId -RoleDefinitionName "Reader" -ResourceGroup $env:resourceGroup
-#     New-AzRoleAssignment -ObjectId $machine.IdentityPrincipalId -RoleDefinitionName "Azure Stack HCI Device Management Role" -ResourceGroup $env:resourceGroup
-#     New-AzRoleAssignment -ObjectId $machine.IdentityPrincipalId -RoleDefinitionName "Azure Connected Machine Resource Manager" -ResourceGroup $env:resourceGroup
-#     $ErrorActionPreference = "Stop"
-# }
-
-# "Insufficient privileges to complete the operation." when using Managed Identity.
-#$spnProviderId = Get-AzADServicePrincipal -DisplayName "Microsoft.AzureStackHCI Resource Provider"
-
-# Construct OU path
 $domainName = $LocalBoxConfig.SDNDomainFQDN.Split('.')
 $ouPath = "OU=$($LocalBoxConfig.LCMADOUName)"
 foreach ($name in $domainName) {
     $ouPath += ",DC=$name"
 }
 
-# Build DNS value
-$dns = "[""" + $LocalBoxConfig.vmDNS + """]"
-
-# Create keyvault name
-$guid = ([System.Guid]::NewGuid()).ToString().subString(0,5).ToLower()
-$keyVaultName = "localbox-kv-" + $guid
-
-# Set physical nodes
-$physicalNodesSettings = "[ "
-$storageAIPs = "[ "
-$storageBIPs = "[ "
-$count = 0
-foreach ($node in $LocalBoxConfig.NodeHostConfig) {
-    if ($count -gt 0) {
-        $physicalNodesSettings += ", "
-        $storageAIPs += ", "
-        $storageBIPs += ", "
-    }
-    $physicalNodesSettings += "{ ""name"": ""$($node.Hostname)"", ""ipv4Address"": ""$($node.IP.Split("/")[0])"" }"
-    $count = $count + 1
-}
-$physicalNodesSettings += " ]"
-$storageAIPs += " ]"
-$storageBIPs += " ]"
-
-# Create diagnostics storage account name
+$guid = ([System.Guid]::NewGuid()).ToString().Substring(0, 5).ToLower()
+$keyVaultName = "localbox-kv-$guid"
 $diagnosticsStorageName = "localboxdiagsa$guid"
 
-# Replace placeholder values in ARM template with real values
+$physicalNodesSettings = @(
+    foreach ($node in $LocalBoxConfig.NodeHostConfig) {
+        @{
+            name        = $node.Hostname
+            ipv4Address = $node.IP.Split('/')[0]
+        }
+    }
+)
+
 $AzLocalParams = "$env:LocalBoxDir\azlocal.parameters.json"
-(Get-Content -Path $AzLocalParams) -replace 'clusterName-staging', $LocalBoxConfig.ClusterName | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'arcNodeResourceIds-staging', $arcNodeResourceIds | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'localAdminUserName-staging', 'Administrator' | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'localAdminPassword-staging', $($LocalBoxConfig.SDNAdminPassword) | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'AzureStackLCMAdminUserName-staging', $($LocalBoxConfig.LCMDeployUsername) | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'AzureStackLCMAdminAdminPassword-staging', $($LocalBoxConfig.SDNAdminPassword) | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'hciResourceProviderObjectID-staging', $env:spnProviderId | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'domainFqdn-staging', $($LocalBoxConfig.SDNDomainFQDN) | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'namingPrefix-staging', $($LocalBoxConfig.LCMDeploymentPrefix) | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'adouPath-staging', $ouPath | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'subnetMask-staging', $($LocalBoxConfig.rbSubnetMask) | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'defaultGateway-staging', $LocalBoxConfig.SDNLabRoute | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'startingIp-staging', $LocalBoxConfig.clusterIpRangeStart | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'endingIp-staging', $LocalBoxConfig.clusterIpRangeEnd | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'dnsServers-staging', $dns | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'keyVaultName-staging', $keyVaultName | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'physicalNodesSettings-staging', $physicalNodesSettings | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'ClusterWitnessStorageAccountName-staging', $env:stagingStorageAccountName | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'diagnosticStorageAccountName-staging', $diagnosticsStorageName | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'storageNicAVLAN-staging', $LocalBoxConfig.StorageAVLAN | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'storageNicBVLAN-staging', $LocalBoxConfig.StorageBVLAN | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'customLocation-staging', $LocalBoxConfig.rbCustomLocationName | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'location-staging', $env:azureLocation | Set-Content -Path $AzLocalParams
-(Get-Content -Path $AzLocalParams) -replace 'tenantId-staging', $env:tenantId | Set-Content -Path $AzLocalParams
+$paramsObject = Get-Content -Path $AzLocalParams -Raw | ConvertFrom-Json -Depth 100
+
+$paramsObject.parameters.clusterName.value = $LocalBoxConfig.ClusterName
+$paramsObject.parameters.arcNodeResourceIds.value = $arcNodeResourceIds
+$paramsObject.parameters.localAdminUserName.value = 'Administrator'
+$paramsObject.parameters.localAdminPassword.value = $LocalBoxConfig.SDNAdminPassword
+$paramsObject.parameters.AzureStackLCMAdminUsername.value = $LocalBoxConfig.LCMDeployUsername
+$paramsObject.parameters.AzureStackLCMAdminPasssword.value = $LocalBoxConfig.SDNAdminPassword
+$paramsObject.parameters.hciResourceProviderObjectID.value = $env:spnProviderId
+$paramsObject.parameters.domainFqdn.value = $LocalBoxConfig.SDNDomainFQDN
+$paramsObject.parameters.namingPrefix.value = $LocalBoxConfig.LCMDeploymentPrefix
+$paramsObject.parameters.adouPath.value = $ouPath
+$paramsObject.parameters.subnetMask.value = $LocalBoxConfig.rbSubnetMask
+$paramsObject.parameters.defaultGateway.value = $LocalBoxConfig.SDNLABRoute
+$paramsObject.parameters.startingIPAddress.value = $LocalBoxConfig.clusterIpRangeStart
+$paramsObject.parameters.endingIPAddress.value = $LocalBoxConfig.clusterIpRangeEnd
+$paramsObject.parameters.dnsServers.value = @($LocalBoxConfig.vmDNS)
+$paramsObject.parameters.keyVaultName.value = $keyVaultName
+$paramsObject.parameters.physicalNodesSettings.value = $physicalNodesSettings
+$paramsObject.parameters.storageNetworkList.value[0].vlanId = $LocalBoxConfig.StorageAVLAN
+$paramsObject.parameters.storageNetworkList.value[1].vlanId = $LocalBoxConfig.StorageBVLAN
+$paramsObject.parameters.clusterWitnessStorageAccountName.value = $env:stagingStorageAccountName
+$paramsObject.parameters.diagnosticStorageAccountName.value = $diagnosticsStorageName
+$paramsObject.parameters.customLocation.value = $LocalBoxConfig.rbCustomLocationName
+$paramsObject.parameters.location.value = $env:azureLocation
+$paramsObject.parameters.tenantId.value = $env:tenantId
+
+$paramsObject | ConvertTo-Json -Depth 100 | Set-Content -Path $AzLocalParams -Encoding utf8
+
+Stop-Transcript
